ad026a70b5c6a811473d584db6771f8e9980c156
hamed kharazmi <hamed.kharazmi@bandpey.com>
2026-01-20 14:36:51 +0100

    Initial Commit

---
+++ MapKit Demo/MapKit_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapKit_DemoApp.swift
+//  MapKit Demo
+//
+//  Created by Hamed Kharazmi on 20.01.26.
+//
+
+import SwiftUI
+
+@main
+struct MapKit_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
