4fe54dc80d7f108132d7695f3d418a5d12f83d16
hamed kharazmi <hamed.kharazmi@bandpey.com>
2026-01-21 14:14:09 +0100

    Added Munich Title View

--- MapKit Demo/MunichView.swift
+++ MapKit Demo/MunichView.swift
@@ -32,11 +32,11 @@ struct MunichView: View {
                 
             }
             
-            ZoomSliderView(delta: $delta)
+            ControlsView(delta: $delta)
         }.onAppear {
             cameraPosition = .region(munichRegion)
         }
-        .onChange(of: delta) { oldValue, newValue in
+        .onChange(of: delta) { _, _ in
             cameraPosition = .region(munichRegion)
         }
     }
@@ -48,6 +48,39 @@ struct ZoomSliderView: View {
     var body: some View {
         Slider(value: $delta, in: 0.05...0.3)
             .padding()
+    }
+}
+
+struct MunichTitleView: View {
+    var body: some View {
+        Text("Explore Munich")
+            .font(.largeTitle)
+            .bold()
+            .padding()
+            .foregroundStyle(
+                LinearGradient(
+                    colors: [.blue, .purple],
+                    startPoint: .leading,
+                    endPoint: .trailing
+                )
+            )
+            .background(.ultraThinMaterial)
+            .clipShape(.rect(cornerRadius: 12))
+            .shadow(radius: 10)
+    }
+}
+
+struct ControlsView: View {
+    @Binding var delta: Double
+    
+    var body: some View {
+        VStack {
+            MunichTitleView()
+            
+            Spacer()
+            
+            ZoomSliderView(delta: $delta)
+        }
     }
 }
 
